4ce437826064836ca113eb20048d07724df9ad8e
Kuljeet-aech <kuljeet.aech@gmail.com>
2023-03-24 19:10:28 +0530

    Update DTO_analysis.py

--- Delivery Time Optimization/DTO_analysis.py
+++ Delivery Time Optimization/DTO_analysis.py
@@ -1,7 +1,11 @@
 import numpy as np
 import pandas as pd
 import plotly.express as px
+<<<<<<< HEAD
 data = pd.read_csv("D:\Kuljeet\Projects\Data-Analysis-Projects\DataSets\Delivery Time Optimization\deliverytime.txt")
+=======
+data = pd.read_csv("DataSets\Delivery time\deliverytime.txt")
+>>>>>>> 851163cae2234ba05cfade6db97f1950df1cb03a
 
 R = 6371
 
@@ -40,4 +44,8 @@ print(data.head())
 #                    color = "Distance",
 #                    trendline="ols", 
 #                    title = "Relationship Between Time Taken and Age")
+<<<<<<< HEAD
 #figure2.show()
+=======
+#figure2.show()
+>>>>>>> 851163cae2234ba05cfade6db97f1950df1cb03a
